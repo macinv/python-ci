bd9f56b2f32a1882e950df41aaacb5982d35084c
Douglas McNeil <dmcneil@mackenzieinvestments.com>
2024-08-19 19:40:16 -0400

    Fix shellcheck warnings

--- scripts/auto_merge_master.sh
+++ scripts/auto_merge_master.sh
@@ -14,11 +14,11 @@ export REPO_URL="https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_REPO_
 
 git --version
 
-git clone --branch ${SOURCE_BRANCH} ${REPO_URL} "${tmp_dir}" 2>&1
+git clone --branch ${SOURCE_BRANCH} "${REPO_URL}" "${tmp_dir}" 2>&1
 cd "${tmp_dir}" || exit 1
 
 echo "setting remote origin.."
-git remote set-url origin ${REPO_URL}
+git remote set-url origin "${REPO_URL}"
 git config --list
 git remote -v
 
@@ -42,4 +42,4 @@ echo "last commits in ${TARGET_BRANCH} after merge:"
 git log -n 3 --oneline
 
 echo "pushing to remote ${TARGET_BRANCH}"
-GIT_TRACE=1 GIT_CURL_VERBOSE=1 git push --verbose ${REPO_URL} ${TARGET_BRANCH}  2>&1
+GIT_TRACE=1 GIT_CURL_VERBOSE=1 git push --verbose "${REPO_URL}" ${TARGET_BRANCH} 2>&1
