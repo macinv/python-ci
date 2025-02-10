9d859044b7dd1f9b02af2b7a22d2a629ebe30436
Douglas McNeil <dmcneil@mackenzieinvestments.com>
2025-02-10 13:58:35 -0500

    First attempt at micromamba-based install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-pip install twine
-pip install build
+python -m pip install twine
+python -m pip install build
 
 python -m build
 twine upload -r "${PYPI_REPO}" -u "${PYPI_USER}" -p "${PYPI_PASSWORD}" dist/*
